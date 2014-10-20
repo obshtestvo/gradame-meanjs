d991beeaff357c27f76b4d0d09afd1133b479784
Valentin Slavov <vnslavov@gmail.com>
2014-10-20 14:59:50 +0300

    moved signals controllers to a directory for better separation of submodules (e.g. location, activities, etc)

---
+++ app/controllers/signals/signals.location.server.controller.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var maxmind = require('maxmind'),
+    _ = require('lodash');
+
+maxmind.init('data/GeoLiteCity.dat');
+
+
+exports.location = function(req, res) {
+  var addr = req.connection.remoteAddress,
+      fwdForHeader = req.headers['x-forwarded-for']
+
+  if (fwdForHeader)
+    addr = fwdForHeader.split(',')[0];
+
+  var location = maxmind.getLocation(addr);
+
+  // set default location to Sofia
+  if (location == null) {
+    location = {
+      countryCode: 'BG',
+      countryName: 'Bulgaria',
+      city: 'Sofia',
+      latitude: 42.6833,
+      longitude: 23.316699999999997,
+    }
+  }
+
+  res.jsonp(location);
+};
+
---
+++ app/controllers/signals/signals.server.controller.js
@@ -0,0 +1,318 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Signal = mongoose.model('Signal'),
+  fs = require('fs'),
+  Q = require('q'),
+  path = require('path'),
+  _ = require('lodash'),
+  SignalModel = require('../../models/signal')
+
+
+/**
+ * Create a signal
+ */
+// TODO: Implement Promises
+exports.create = function(req, res) {
+
+  var location = req.body.location;
+
+  if(location && location.length){
+    location = location.substr(1,location.length-2);
+    req.body.location = location.split(', ');
+  }
+
+  var signal = new Signal(req.body);
+
+  if(req.user && req.user.id){
+    signal.author = req.user.id;
+  }
+
+  signal.save(function(err) {
+    if (err) {
+      res.jsonp({
+        errors: err.errors,
+        signal: signal
+      });
+    } else {
+
+      // populate files
+      if(req.files){
+
+        var signalPath = path.join(__dirname , "/../../public/img/signals/", signal['_id']+'');
+        var images = [];
+        if(!fs.existsSync(path.join(__dirname , "/../../public/img/signals"))){
+          fs.mkdirSync(path.join(__dirname , "/../../public/img/signals"));
+        }
+
+        if(!fs.existsSync(signalPath) || !fs.statSync(signalPath).isDirectory()){
+          fs.mkdirSync(signalPath);
+        }
+
+        var index = 0;
+        for(var i in req.files){
+          var file = req.files[i];
+          var ext = file.name.substr(file.name.lastIndexOf('.'));
+          var newPath = signalPath+'/'+index+ext;
+          images.push(''+index+ext);
+          var fileData = fs.readFileSync(file.path);
+          fs.writeFileSync(newPath, fileData);
+          fs.unlinkSync(file.path);
+          index++;
+        }
+
+        signal.images = images;
+
+        signal.save(function(){
+          if (err) {
+            res.jsonp({
+              errors: err.errors,
+              signal: signal
+            });
+          } else {
+            res.jsonp(signal);
+          }
+        });
+      } else {
+        res.jsonp(signal);
+      }
+    }
+  });
+};
+
+/**
+ * Show the current signal
+ */
+exports.read = function(req, res) {
+  res.jsonp(req.signal);
+};
+
+/**
+ * Update a signal
+ */
+exports.update = function(req, res) {
+  var signal = req.signal;
+
+  signal = _.extend(signal, req.body);
+
+  signal.save(function(err) {
+    if (err) {
+      res.render('error', {
+        status: 500
+      });
+    } else {
+      res.jsonp(signal);
+    }
+  });
+};
+
+/**
+ * Delete an signal
+ */
+exports.delete = function(req, res) {
+  var signal = req.signal;
+
+  signal.remove(function(err) {
+    if (err) {
+      res.render('error', {
+        status: 500
+      });
+    } else {
+      res.jsonp(signal);
+    }
+  });
+};
+
+/**
+ * List of Signals
+ */
+exports.list = function(req, res) {
+  var queryJson = {};
+
+  if(req.query){
+    if(req.query.bounds){
+      var bounds = req.query.bounds;
+
+      bounds = bounds.substr(1,bounds.length-2).split('), (');
+      console.log(bounds);
+
+      bounds[0] = bounds[0].substr(1,bounds[0].length-1).split(', ');
+      bounds[1] = bounds[1].substr(0,bounds[1].length-2).split(', ');
+
+
+      queryJson['location'] = { $geoWithin : { $box : bounds} };
+      //console.log(bounds);
+
+    }
+    if(req.query.type){
+      queryJson['type'] = req.query.type;
+    }
+    if(req.query.status){
+      queryJson['status'] = req.query.status;
+    }
+
+  }
+
+
+  Signal.find(queryJson).sort('-date_created').limit(req.query.limit ? req.query.limit : 0).populate('user', 'displayName').exec(function(err, signals) {
+    if (err) {
+      res.jsonp('error', {
+        status: 500,
+        err: err
+      });
+    } else {
+
+      res.jsonp(signals);
+
+    }
+  });
+};
+
+exports.mine = function(req, res) {
+  var queryJson = {};
+
+  queryJson['author'] = req.user._id;
+  console.log(req.user._id);
+
+  if (req.query) {
+
+    if (req.query.type) {
+      queryJson['type'] = req.query.type;
+    }
+    if(req.query.status){
+      queryJson['status'] = req.query.status;
+    }
+
+  }
+
+
+  Signal.find(queryJson).sort('-date_created').limit(req.query.limit ? req.query.limit : 0).populate('user', 'displayName').exec(function(err, signals) {
+    if (err) {
+      res.jsonp('error', {
+        status: 500,
+        err: err
+      });
+    } else {
+
+      res.jsonp(signals);
+
+    }
+  });
+};
+
+exports.constants = function(req, res) {
+  res.jsonp(SignalModel.constants)
+}
+
+//@TODO implement
+exports.assign = function(req, res) {
+  var signal = req.signal;
+}
+
+//@TODO implement
+exports.unassign = function(req, res) {
+  var signal = req.signal;
+}
+
+exports.findNear = function(req, res) {
+
+  var location = req.query.location;
+
+  if(location && location.length){
+    location = location.substr(1,location.length-2);
+    req.query.location = location.split(', ');
+  }
+
+  if(!location.length){
+    console.log({err: 'Location is malformed',location: location});
+    res.jsonp('error', {
+      status: 500
+    });
+  } else {
+    // $maxDistance: 0.00019
+    Signal.find({location: { $nearSphere: req.query.location, $maxDistance: 0.00025} }, function(err, signals) {
+      if (err) {
+        res.jsonp('error', {
+          status: 500
+        });
+      } else {
+
+        res.jsonp(signals);
+
+      }
+    });
+  }
+
+};
+
+
+exports.activitiesAdd = function(req, res){
+  
+  var signal = req.signal;
+
+  if(!signal.acitvities)
+    signal.acitvities = [];
+
+  var activity = _.extend({}, req.body);
+  if(req.user && req.user.id){
+    activity.createdBy = req.user.id;
+  }
+
+  signal.activities.push(activity);
+
+  console.log(signal);
+
+  signal.save(function(err) {
+    if (err) {
+      res.render('error', {
+        status: 500
+      });
+    } else {
+      res.jsonp(signal);
+    }
+  });
+}
+
+/**
+ * Signal middleware
+ */
+exports.signalByID = function(req, res, next, id) {
+  Signal.load(id, function(err, signal) {
+    if (err && (err.name && err.name!='CastError')) return next(err);
+    if (!signal) return res.send(404, 'No such signal');
+    req.signal = signal;
+    next();
+  });
+};
+
+/**
+ * Signal ownership middleware
+ */
+exports.hasOwnership = function(req, res, next) {
+  if (req.signal.created_by.id !== req.user.id) {
+    return res.send(403, 'User is not authorized');
+  }
+  next();
+};
+
+
+/**
+ * A signal self-assignment middleware
+ */
+exports.requiresAssignmentUnlessSuper = function(req, res, next) {
+  if (req.user.isSuper()) next()
+  var hasAssignment = false;
+  _.each(req.signal.handled_by, function(assignment) {
+    if (assignment.user.id == req.user.id) {
+      hasAssignment = true;
+      return false;
+    }
+  })
+  if (!hasAssignment) {
+    return res.send(403, 'User is has nothing to do with this signal');
+  }
+  next();
+};
