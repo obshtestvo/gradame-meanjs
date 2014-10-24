ed1687e4976d729b0b10af38916be24302cfffdf
Valentin Slavov <vnslavov@gmail.com>
2014-10-24 15:23:40 +0300

    manage navigation menu with menus service

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -2,11 +2,18 @@
 
 angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus',
 	function($scope, Authentication, Menus) {
+    //this.addMenuItem = function(menuId, menuItemTitle, menuItemURL, menuItemType, menuItemUIRoute, isPublic, roles, position) {
+    Menus.addMenuItem('topbar', 'Начало', '/', 'item', 'home-dash', true, '*');
+    Menus.addMenuItem('topbar', 'Сигнали', 'signals/', 'item', 'signals.index', true, '*');
+    Menus.addMenuItem('topbar', 'Подай сигна', 'signals/new', 'item', 'signals.new', true, '*');
+
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
 
-		$scope.toggleCollapsibleMenu = function() {
+    console.log($scope.menu.items);
+
+    $scope.toggleCollapsibleMenu = function() {
 			$scope.isCollapsed = !$scope.isCollapsed;
 		};
 
