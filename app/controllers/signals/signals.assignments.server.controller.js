70973447f323f287917c79ff942f51664be625ad
Anton Stoychev <antitoxic@gmail.com>
2014-10-25 17:55:47 +0300

    fixes on backend assignment logic

--- app/controllers/signals/signals.assignments.server.controller.js
+++ app/controllers/signals/signals.assignments.server.controller.js
@@ -10,19 +10,18 @@ exports.create = function(req, res, next) {
   signal.assignments.push(assignment)
   signal.save(function(err) {
     if (err) return next(err)
-    res.jsonp(signal);
+    res.jsonp(assignment);
   });
 }
 
-// @TODO implement
-exports.delete = function(req, res) {
+exports.delete = function(req, res, next) {
   var signal = req.signal;
-  signal.handled_by = signal.handled_by.filter(function(assignment) {
+  signal.assignments = signal.assignments.filter(function(assignment) {
     return !assignment.user._id.equals(req.targetUser._id)
   })
   signal.save(function(err) {
     if (err) return next(err)
-    res.jsonp(signal);
+    res.jsonp({});
   });
 }
 
